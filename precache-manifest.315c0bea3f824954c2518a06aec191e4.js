7900d10901389d6308fe16de8a04e56bdd1b2a59
Kyle Case <casekw@gmail.com>
2020-10-14 17:04:52 -0500

    Updates

---
+++ precache-manifest.315c0bea3f824954c2518a06aec191e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87eca5ca407eb2215299ba4fe778522a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec13f8ec6ea61c0305c3",
+    "url": "/static/css/main.ac567560.chunk.css"
+  },
+  {
+    "revision": "9e65d2ef4642eb1108d0",
+    "url": "/static/js/2.61ba9ea2.chunk.js"
+  },
+  {
+    "revision": "f7e779f4617dfd9c3d5d399465296ab3",
+    "url": "/static/js/2.61ba9ea2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec13f8ec6ea61c0305c3",
+    "url": "/static/js/main.46259ae1.chunk.js"
+  },
+  {
+    "revision": "253093df91c0ca845abe",
+    "url": "/static/js/runtime-main.b6142808.js"
+  }
+]);
