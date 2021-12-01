06dc0f9fc807090b50764ef4896698a68b4d845c
Kyle Case <kcase@hey.com>
2021-12-01 09:35:33 -0600

    Updates

---
+++ precache-manifest.53ea91bc7d366e94a46984ca0aadfc7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "482f3e405df2fae926a323283697af16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acf1bc944b4fe873fe55",
+    "url": "/static/css/main.8b1699d0.chunk.css"
+  },
+  {
+    "revision": "70590a017366da41ff99",
+    "url": "/static/js/2.afbb394e.chunk.js"
+  },
+  {
+    "revision": "78a4d6617f487affe00d25c6fd180987",
+    "url": "/static/js/2.afbb394e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acf1bc944b4fe873fe55",
+    "url": "/static/js/main.04586edb.chunk.js"
+  },
+  {
+    "revision": "55cf03ad92c385910fbc",
+    "url": "/static/js/runtime-main.e40405f4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b4934de9eb1f59883bbd235719c6d7de.js"
+  "/precache-manifest.53ea91bc7d366e94a46984ca0aadfc7c.js"
 );
 
 self.addEventListener('message', (event) => {
